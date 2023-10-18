4684547b08e9bb8fa7c744bea0d1a46a4c2b2a85
Roy Stogner <Roy.Stogner@inl.gov>
2023-10-18 13:17:31 -0500

    Add ignore/restore_warnings.h headers
    VexCL doesn't meet our paranoid-warnings standards.
    
    At some point I'll run into a 3rd party code that does.  One day.

---
+++ src/utilities/include/metaphysicl/ignore_warnings.h
@@ -0,0 +1,129 @@
+//-----------------------------------------------------------------------bl-
+//--------------------------------------------------------------------------
+//
+// MetaPhysicL - A metaprogramming library for physics calculations
+//
+// Copyright (C) 2013 The PECOS Development Team
+//
+// This library is free software; you can redistribute it and/or
+// modify it under the terms of the Version 2.1 GNU Lesser General
+// Public License as published by the Free Software Foundation.
+//
+// This library is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+//
+// You should have received a copy of the GNU Lesser General Public
+// License along with this library; if not, write to the Free Software
+// Foundation, Inc. 51 Franklin Street, Fifth Floor,
+// Boston, MA  02110-1301  USA
+//
+//-----------------------------------------------------------------------el-
+//
+// $Id$
+//
+//--------------------------------------------------------------------------
+
+// Note: no include guards!  We want to be able to #include this
+// header multiple times.
+
+// TODO: icpc options
+#ifdef __clang__
+#pragma clang diagnostic push
+// The first four should remain here to avoid warnings about warnings
+#pragma clang diagnostic ignored "-Wunknown-pragmas"
+#pragma clang diagnostic ignored "-Wpragmas"
+#pragma clang diagnostic ignored "-Wunknown-warning-option"
+#pragma clang diagnostic ignored "-Wunknown-warning"
+#pragma clang diagnostic ignored "-Wunused-parameter"
+#pragma clang diagnostic ignored "-Wextra-semi"
+#pragma clang diagnostic ignored "-Wvariadic-macros"
+#pragma clang diagnostic ignored "-Wc++11-extensions"
+#pragma clang diagnostic ignored "-Wnested-anon-types"
+#pragma clang diagnostic ignored "-Wsign-compare"
+#pragma clang diagnostic ignored "-Wunused-private-field"
+#pragma clang diagnostic ignored "-Wextra"
+#pragma clang diagnostic ignored "-Wredundant-decls"
+#pragma clang diagnostic ignored "-Wcast-align"
+#pragma clang diagnostic ignored "-Wcast-qual"
+#pragma clang diagnostic ignored "-Wswitch-default"
+#pragma clang diagnostic ignored "-Wdeprecated-declarations"
+#pragma clang diagnostic ignored "-Woverloaded-virtual"
+#pragma clang diagnostic ignored "-Wmacro-redefined"
+#pragma clang diagnostic ignored "-Winconsistent-missing-override"
+#pragma clang diagnostic ignored "-Wunused-but-set-variable"
+#pragma clang diagnostic ignored "-Winvalid-utf8"
+// Ignore warnings from code that does "if (foo) bar();"
+#pragma clang diagnostic ignored "-Wmisleading-indentation"
+#pragma clang diagnostic ignored "-Wint-in-bool-context"
+#pragma clang diagnostic ignored "-Wdeprecated-copy"
+#endif
+
+#if defined(__GNUC__) && !defined(__INTEL_COMPILER) && !defined(__clang__)
+// GCC > 4.5 supports diagnostic pragmas with push/pop
+#if (__GNUC__ > 4) || (__GNUC__ == 4 && __GNUC_MINOR__ > 5)
+#pragma GCC diagnostic push
+// The first four should remain here to avoid warnings about warnings
+#pragma GCC diagnostic ignored "-Wunknown-pragmas"
+#pragma GCC diagnostic ignored "-Wpragmas"
+#pragma GCC diagnostic ignored "-Wunknown-warning-option"
+#pragma GCC diagnostic ignored "-Wunknown-warning"
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+// These two don't work?
+#pragma GCC diagnostic ignored "-Wpedantic"
+#pragma GCC diagnostic ignored "-Wdeprecated"
+// But this is helpful with some MPI stacks
+#pragma GCC diagnostic ignored "-Wunused-parameter"
+// And these are for cppunit
+#pragma GCC diagnostic ignored "-Woverloaded-virtual"
+#pragma GCC diagnostic ignored "-Wundef"
+// And these are for Trilinos
+#pragma GCC diagnostic ignored "-Wextra"
+#pragma GCC diagnostic ignored "-Wshadow"
+// And these are for PETSc
+#pragma GCC diagnostic ignored "-Wredundant-decls"
+#pragma GCC diagnostic ignored "-Wcast-qual"
+#pragma GCC diagnostic ignored "-Wswitch-default"
+// And these are for Eigen
+#pragma GCC diagnostic ignored "-Wconversion"
+#pragma GCC diagnostic ignored "-Wstack-protector"
+#pragma GCC diagnostic ignored "-Wmaybe-uninitialized"
+// And this for VTK
+#pragma GCC diagnostic ignored "-Wlogical-op"
+// Ignore warnings from code that uses deprecated members of std, like std::auto_ptr.
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+// This is mostly for the (deprecated...) C++ MPI wrappers
+#pragma GCC diagnostic ignored "-Wsuggest-override"
+// Ignore this for VTK
+#pragma GCC diagnostic ignored "-Wfloat-conversion"
+// Ignore warnings from code that does "if (foo) bar();"
+#pragma GCC diagnostic ignored "-Wmisleading-indentation"
+// Ignore warnings from bad placement new use
+#pragma GCC diagnostic ignored "-Wplacement-new"
+#pragma GCC diagnostic ignored "-Wint-in-bool-context"
+#pragma GCC diagnostic ignored "-Wimplicit-fallthrough"
+#pragma GCC diagnostic ignored "-Wparentheses"
+#pragma GCC diagnostic ignored "-Wcast-function-type"
+#pragma GCC diagnostic ignored "-Wdeprecated-copy"
+#pragma GCC diagnostic ignored "-Wclass-memaccess"
+#endif
+#endif // __GNUC__ && !__INTEL_COMPILER
+
+#ifdef __NVCOMPILER
+#  pragma nv_diagnostic push
+// Warning numbers are from --display_error_number
+#  ifdef __NVCC_DIAG_PRAGMA_SUPPORT__
+#    pragma nv_diag_suppress 11 // unrecognized preprocessing directive
+#    pragma nv_diag_suppress 177 // declared but never referenced
+#    pragma nv_diag_suppress 541 // allowing all exceptions is incompatible with previous function
+#    pragma nv_diag_suppress 1758 // conversion between incompatible vector types
+#    pragma nv_diag_suppress = integer_sign_change
+#  else
+#    pragma diag_suppress 11
+#    pragma diag_suppress 177
+#    pragma diag_suppress 541
+#    pragma diag_suppress 1758
+#    pragma diag_suppress = integer_sign_change
+#  endif
+#endif
